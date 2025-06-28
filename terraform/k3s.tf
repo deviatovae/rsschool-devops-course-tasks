a1f320f20cdebf044938136ebf664edd763f8047
Alena Deviatova <aedeviatova@yandex.ru>
2025-06-28 00:53:16 -0400

    run format

--- terraform/k3s.tf
+++ terraform/k3s.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "k3s_server" {
-  ami           = var.ami
-  instance_type = "t3.small"
-  subnet_id     = aws_subnet.private_a.id
-  key_name      = var.key_name
+  ami                    = var.ami
+  instance_type          = "t3.small"
+  subnet_id              = aws_subnet.private_a.id
+  key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.private_sg.id]
 
   user_data = file("${path.module}/user_data/server.sh")
@@ -21,7 +21,7 @@ resource "aws_instance" "k3s_agent" {
 
   user_data = templatefile("${path.module}/user_data/agent.sh.tftpl", {
     k3s_server_private_ip = aws_instance.k3s_server.private_ip
-    k3s_token = var.k3s_token
+    k3s_token             = var.k3s_token
   })
 
   tags = {
